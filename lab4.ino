fa04bc3283e0f1b3bbb20e45c7d4c042fcc7e902
Wynn Drahorad <wynn.drahorad@gmail.com>
2018-04-23 13:23:14 -0500

    add mouse library to send mouse movements when button exceeds specific threshold

--- lab4.ino
+++ lab4.ino
@@ -1,149 +1,55 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include "Adafruit_BLE.h"
-#include "Adafruit_BluefruitLE_SPI.h"
-#include "Adafruit_BluefruitLE_UART.h"
+#include <CapacitiveSensor.h>
+#include <Mouse.h>
 
-#include "BluefruitConfig.h"
+const int LEFT = 0;
+const int TOP = 1;
+const int RIGHT = 2;
+const int DOWN = 3;
+const int MOVE_SPEED = 20;
+long total0, total1, total2, total3, start;
 
-#if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>
-#endif
+CapacitiveSensor   cs_5_10 = CapacitiveSensor(5,10);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_5_11 = CapacitiveSensor(5,11);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_5_12 = CapacitiveSensor(5,12);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_5_13 = CapacitiveSensor(5,13);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
 
-/*=========================================================================
-    APPLICATION SETTINGS
-
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    -----------------------------------------------------------------------*/
-    #define FACTORYRESET_ENABLE         0
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-/*=========================================================================*/
-
-Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-// A small helper
-void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
-}
-
-void setup() {
-  // put your setup code here, to run once:
-void setup(void)
+void setup()                    
 {
-  while (!Serial);  // required for Flora & Micro
-  delay(500);
-
-  Serial.begin(115200);
-
-  /* Initialise the module */
-  Serial.print(F("Initialising the Bluefruit LE module: "));
-
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
-  Serial.println( F("OK!") );
-
-  if ( FACTORYRESET_ENABLE )
-  {
-    /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ){
-      error(F("Couldn't factory reset"));
-    }
-  }
-
-  /* Disable command echo from Bluefruit */
-  ble.echo(false);
-
-  Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
-  ble.info();
-
-  /* Change the device name to make it easier to find */
-  Serial.println(F("Setting device name to 'Bluefruit Keyboard': "));
-  if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=Bluefruit Keyboard" )) ) {
-    error(F("Could not set device name?"));
-  }
-
-  /* Enable HID Service */
-  Serial.println(F("Enable HID Service (including Keyboard): "));
-  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
-  {
-    if ( !ble.sendCommandCheckOK(F( "AT+BleHIDEn=On" ))) {
-      error(F("Could not enable Keyboard"));
-    }
-  }else
-  {
-    if (! ble.sendCommandCheckOK(F( "AT+BleKeyboardEn=On"  ))) {
-      error(F("Could not enable Keyboard"));
-    }
-  }
-
-  /* Add or remove service requires a reset */
-  Serial.println(F("Performing a SW reset (service changes require a reset): "));
-  if (! ble.reset() ) {
-    error(F("Couldn't reset??"));
-  }
-
-  Serial.println();
-  Serial.println(F("Go to your phone's Bluetooth settings to pair your device"));
-  Serial.println(F("then open an application that accepts keyboard input"));
-
-  Serial.println();
-  Serial.println(F("Enter the character(s) to send:"));
-  Serial.println(F("- \\r for Enter"));
-  Serial.println(F("- \\n for newline"));
-  Serial.println(F("- \\t for tab"));
-  Serial.println(F("- \\b for backspace"));
-
-  Serial.println();
+   cs_5_10.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+   Mouse.begin();
+   start = millis();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  // Display prompt
-  Serial.print(F("keyboard > "));
+void loop()                    
+{
+   total0 =  cs_5_10.capacitiveSensor(30);
+   total1 =  cs_5_11.capacitiveSensor(30);
+   total2 =  cs_5_12.capacitiveSensor(30);
+   total3 =  cs_5_13.capacitiveSensor(30);
 
-  // Check for user input and echo it back if anything was found
-  char keys[BUFSIZE+1];
-  getUserInput(keys, BUFSIZE);
+    /*  
+    total 0 is up
+    total 1 is right
+    total 2 is down
+    total 3 is left
+    */
 
-  Serial.print("\nSending ");
-  Serial.println(keys);
+    if (total0 > 1000) {
+      Serial.println("moving up");
+      Mouse.move(0, -MOVE_SPEED, 0);
+    }
+    if (total1 > 1000) {
+      Serial.println("moving right");
+      Mouse.move(MOVE_SPEED, 0, 0);
+    }
+    if (total2 > 2000) {
+      Serial.println("moving down");
+      Mouse.move(0, MOVE_SPEED, 0);
+    }
+    if (total3 > 1500) {
+      Serial.println("moving left");
+      Mouse.move(-MOVE_SPEED, 0, 0);
+    }
 
-  ble.print("AT+BleKeyboard=");
-  ble.println(keys);
-
-  if( ble.waitForOK() )
-  {
-    Serial.println( F("OK!") );
-  }else
-  {
-    Serial.println( F("FAILED!") );
-  }
 }
